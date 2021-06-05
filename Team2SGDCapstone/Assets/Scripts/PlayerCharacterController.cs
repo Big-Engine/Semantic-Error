7ca7fef39f197b8c5f449ce93d45b6a2fe3133ab
Big-Engine <71451068+Big-Engine@users.noreply.github.com>
2021-06-05 02:44:51 -0400

    Wall Jumping & Tweaks
    The player can now wall jump. Move towards a wall to slide down it and press space to leap away.
    Modified the player's normal jump so it lasts as long as the player holds down space. Tap space for a short hop and hold it to do a full jump.
    The player also now has a few more variables. They can be froze (i.e. can't move) and their double jump can be locked.

--- Team2SGDCapstone/Assets/Scripts/PlayerCharacterController.cs
+++ Team2SGDCapstone/Assets/Scripts/PlayerCharacterController.cs
@@ -4,66 +4,152 @@ using UnityEngine;
 
 public class PlayerCharacterController : MonoBehaviour
 {
+    [Header("Player Velocity")]
     private float inputDirection;
     private Vector3 moveVector;
-    [SerializeField] private Vector3 velocity;
+    private Vector3 velocity;
     [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private float fallForce = -10.0f;
+    private CharacterController characterController;
+
+    [Header("Player Controls")]
+    //isActive determines if the player can move at all. Use it to freeze the player.
+    public bool isActive = true;
+    [SerializeField] private bool isWallJumping = false;
     public float playerSpeed = 10.0f;
     public float jumpForce = 10.0f;
-    private bool canDoubleJump = false;
+    public bool doubleJumpEnabled = false;
+    [SerializeField] private bool canDoubleJump = false;
+
+    //These are used to check if the player is standing on anything in the "Ground" Layer.
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
     private bool isGrounded;
 
-    private CharacterController characterController;
+    //Player Animations
     Animator animator;
 
     void Start()
     {
         characterController = GetComponent<CharacterController>();
-        //animator = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
+        //This casts a sphere at the bottom of the player. If they're on anything in the Ground layer, this becomes true.
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
         if(isGrounded && velocity.y < 0)
+        //This resets the player's downward velocity to -5.0f so they fall quickly when walking off ledges.
         {
             velocity.y = -5.0f;
-            canDoubleJump = true;
+            CheckForDoubleJumpPermisson();
         }
         
-        inputDirection = Input.GetAxis("Horizontal");
-        moveVector = new Vector3(inputDirection, 0f, 0f);
-        characterController.Move(moveVector * playerSpeed * Time.deltaTime);
 
-        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+
+        
+        if(isActive && !isWallJumping)
+        //If the player is allowed to move, use InputDirection to go left and right.
         {
-            velocity.y = Mathf.Sqrt(jumpForce * -2.0f * gravity);
-            canDoubleJump = true;
+            inputDirection = Input.GetAxis("Horizontal");
+            //moveVector = new Vector3(inputDirection, 0f, 0f);
+        }
+        
+        if (!isActive)
+        //Don't allow the player to move.
+        {
+            //moveVector = new Vector3(0, 0, 0);
+            inputDirection = 0;
         }
 
-        if(Input.GetKeyDown(KeyCode.Space) && !isGrounded && canDoubleJump)
+        moveVector = new Vector3(inputDirection, 0f, 0f);
+
+        //Applies horizontal forces to the player.
+        characterController.Move(moveVector * playerSpeed * Time.deltaTime);
+
+        //This causes the player to jump when standing on the ground.
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded && isActive)
+        {
+            velocity.y = Mathf.Sqrt(jumpForce * -2.0f * gravity);
+            CheckForDoubleJumpPermisson();
+        }
+
+        //This causes a double jump if the player is not on the ground but has a reserved double jump.
+        if(Input.GetKeyDown(KeyCode.Space) && !isGrounded && canDoubleJump && isActive)
         {
             velocity.y = Mathf.Sqrt(jumpForce * -2.0f * gravity);
             canDoubleJump = false;
         }
 
+        //If the player lets go of the jump button before they reach the peak of their jump, they will instead
+        //prematurely end their jump (Basically allows short jumps).
+        if(velocity.y > 0 && !Input.GetKey(KeyCode.Space) && isActive)
+        {
+            velocity.y += fallForce * Time.deltaTime;
+        }
+
+        //This pulls the player downwards.
         velocity.y += gravity * Time.deltaTime;
+
+        AnimationHandler();
+        //Applies vertical forces to the player.
         characterController.Move(velocity * Time.deltaTime);
     }
 
-    //private void OnControllerColliderHit(ControllerColliderHit hit)
-    //{
-    //    if (characterController.collisionFlags == CollisionFlags.Sides)
-    //    {
-    //        if (Input.GetKeyDown(KeyCode.Space))
-    //        {
-    //            moveVector = hit.normal * playerSpeed;
-    //            moveVector.y = jumpForce;
-    //            canDoubleJump = true;
-    //        }
-    //    }
-    //}
+    private void AnimationHandler()
+    //Handles the animations. Leave this alone or risk breaking everything!
+    {
+        bool isWalkingRight = animator.GetBool("isWalkingRight");
+
+        if(inputDirection > 0.1 && !isWalkingRight)
+        {
+            animator.SetBool("isWalkingRight", true);
+        }
+        else if (inputDirection == 0 && isWalkingRight)
+        {
+            animator.SetBool("isWalkingRight", false);
+        }
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    //This handles events for colliding with other objects.
+    {
+        if (!characterController.isGrounded && hit.normal.y < 0.1f)
+        //If the player is touching a wall and they're not on the ground...
+        {
+            if(velocity.y < 0)
+            //Check if the player has negative velocity and set it to -5 so they slide down walls.
+            {
+                velocity.y = -5.0f;
+            }
+
+            if(Input.GetKeyDown(KeyCode.Space))
+            //And if they press jump...
+            {
+                //They perform a wall jump and reenable their double jump.
+                velocity.y = Mathf.Sqrt((jumpForce/2) * -2.0f * gravity);
+
+                inputDirection = hit.normal.x;
+                isWallJumping = true;
+                StartCoroutine(WallJumpCoroutine());
+            }
+        }
+    }
+
+    IEnumerator WallJumpCoroutine()
+    //Freezes the player for a brief moment to allow the wall jump to push them back.
+    {
+        yield return new WaitForSeconds(0.3f);
+        isWallJumping = false;
+    }
+
+    private void CheckForDoubleJumpPermisson()
+    {
+        if(doubleJumpEnabled)
+        {
+            canDoubleJump = true;
+        }
+    }
 }
