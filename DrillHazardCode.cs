0a667b144a6e8c606e19ccc3a85bf1ad3c03f5e5
JokerChris777 <85379255+JokerChris777@users.noreply.github.com>
2021-06-08 18:20:11 -0400

    Add files via upload

---
+++ DrillHazardCode.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrillHazardCode : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        yield return new WaitforSeconds(6f);
+        transform.Translate(Vector3.down * fallSpeed * Time.deltaTime, Space.World);
+        transform.Rotate(Vector3.forward, spinSpeed * Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
