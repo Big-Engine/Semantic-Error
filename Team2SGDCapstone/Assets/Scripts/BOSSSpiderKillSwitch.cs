bc170a82bcc70c679949bddc3114ed461a2cea9b
ZGiraldo <70169592+ZGiraldo@users.noreply.github.com>
2021-06-25 17:30:52 -0400

    Spider boss (functional)
    Spider boss pretty much fully functional. May need additional tweaks and/or adjustments

---
+++ Team2SGDCapstone/Assets/Scripts/BOSSSpiderKillSwitch.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BOSSSpiderKillSwitch : MonoBehaviour
+{
+    public Transform SpiderBoss;
+    private BOSS_Spider spiderBossScript;
+
+    void OnEnable()
+    {
+        EventManager.OnReset += ResetPosition;
+    }
+
+    void OnDisable()
+    {
+        EventManager.OnReset -= ResetPosition;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spiderBossScript = SpiderBoss.GetComponent<BOSS_Spider>();
+        gameObject.GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    //trigger the event in the parent script, disable this specific objects box trigger (to prevent from being triggered again)
+    {
+        if (other.tag == "Player")
+        {
+            spiderBossScript.DeathAnimation();
+            gameObject.GetComponent<Renderer>().material.color = Color.green;
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+
+    private void ResetPosition()
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.red;
+        gameObject.GetComponent<BoxCollider>().enabled = true;
+    }
+}
--- Team2SGDCapstone/Assets/Scripts/BOSS_Spider.cs
+++ Team2SGDCapstone/Assets/Scripts/BOSS_Spider.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class BOSS_Spider : MonoBehaviour
 {
     public Vector3 endPosition;
+    public Vector3 initialPosition;
     private Vector3 lastPosition;
     [SerializeField] float speed;
+
+    private bool fallingStart;
+    private bool isMoving;
+    private bool isIdle;
+    private Animator spiderAnim;
 
     void OnEnable()
     {
@@ -20,16 +26,62 @@ public class BOSS_Spider : MonoBehaviour
 
     void Start()
     {
-        lastPosition = transform.position;//initial position
+        fallingStart = true;
+        isIdle = false;
+        spiderAnim = GetComponentInChildren<Animator>();
+        spiderAnim.SetBool("isScreaming", true); //play scream animation  
+        Invoke("StartCycle", 7.0f);
     }
 
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, endPosition, speed * Time.deltaTime);
+        if (fallingStart)
+        {
+            transform.position = Vector3.Lerp(transform.position, initialPosition, 3 * Time.deltaTime);
+        }
+        else
+        {
+            if(isMoving)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, endPosition, speed * Time.deltaTime);
+            }
+            if (Vector3.Distance(transform.position, endPosition) < 1f && !isIdle)
+            {
+                IdleAnimation();
+                isIdle = true;
+            }
+                
+        }
+    }
+
+    void StartCycle()
+    {
+        lastPosition = transform.position;//initial position
+        spiderAnim.SetBool("isScreaming", false);
+        spiderAnim.SetBool("isWalking", true);
+        fallingStart = false;
+        isMoving = true;
+    }
+
+    public void DeathAnimation()
+    {
+        //play death animation
+        isMoving = false;
+        spiderAnim.SetBool("isIdle", false);
+        spiderAnim.SetBool("isWalking", false);
+        spiderAnim.SetBool("isDying", true);              
+        GetComponent<BoxCollider>().isTrigger = false;
+    }
+
+    void IdleAnimation()
+    {
+        //when spider reaches end
+        spiderAnim.SetBool("isWalking", false);
+        spiderAnim.SetBool("isIdle", true);
     }
 
     void ResetPosition()
     {
-
+        transform.position = lastPosition;
     }
 }
